85eeafd7912cbf59b2ce01744523ba7b02e7caae
cassiobotaro <cassiobotaro@gmail.com>
2017-12-18 20:45:36 -0200

    removendo target

--- guessing_game/target/debug/deps/guessing_game-a419e7cf92e04dd6.d
+++
@@ -1,5 +0,0 @@
-/home/cassiobotaro/rust/rust_playground/guessing_game/target/debug/deps/guessing_game-a419e7cf92e04dd6: src/main.rs
-
-/home/cassiobotaro/rust/rust_playground/guessing_game/target/debug/deps/guessing_game-a419e7cf92e04dd6.d: src/main.rs
-
-src/main.rs:
--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/home/cassiobotaro/rust/rust_playground/guessing_game/target/debug/guessing_game: /home/cassiobotaro/rust/rust_playground/guessing_game/src/main.rs
